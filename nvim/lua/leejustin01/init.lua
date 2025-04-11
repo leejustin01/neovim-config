190ab533eea1aa3ff92a3803cbfa852a7f2a09e8
Justin Lee <93400903+leejustin01@users.noreply.github.com>
2025-04-11 11:32:59 -0700

    Initial commit: neovim config

---
+++ nvim/lua/leejustin01/init.lua
@@ -0,0 +1,3 @@
+require("leejustin01.remap")
+require("leejustin01.set")
+require("leejustin01.lazy")
---
+++ nvim/lua/leejustin01/lazy.lua
@@ -0,0 +1,136 @@
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+local plugins = {
+  { 
+    'morhetz/gruvbox', 
+    config = function()
+      vim.cmd.colorscheme("gruvbox") 
+    end 
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        highlight = { enable = true },
+        indent = { enable = true },
+      }
+    end
+  },
+
+  { 'nvim-treesitter/playground' },
+  { 'theprimeagen/harpoon' },
+  { 'mbbill/undotree' },
+  { 'tpope/vim-fugitive' },
+  { 'neovim/nvim-lspconfig' },
+
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('telescope').setup()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-buffer", -- source for text in buffer
+        "hrsh7th/cmp-path", -- source for file system paths
+        {
+            "L3MON4D3/LuaSnip",
+            version = "v2.*",
+            -- install jsregexp (optional!).
+            build = "make install_jsregexp",
+        },
+        "rafamadriz/friendly-snippets",
+        "onsails/lspkind.nvim", -- vs-code like pictograms
+    },
+    config = function()
+        local cmp = require("cmp")
+        local lspkind = require("lspkind")
+        local luasnip = require("luasnip")
+
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.close(),
+                ["<CR>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true,
+                }),
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "buffer" },
+                { name = "path" },
+            }),
+        })
+
+        vim.cmd([[
+      set completeopt=menuone,noinsert,noselect
+      highlight! default link CmpItemKind CmpItemMenuDefault
+    ]])
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        require("mason").setup()
+
+        require("mason-lspconfig").setup({
+            automatic_installation = true,
+            ensure_installed = {
+                "cssls",
+                "eslint",
+                "html",
+                "jsonls",
+                "ts_ls",
+                "pyright",
+                "tailwindcss",
+            },
+        })
+
+        require("mason-tool-installer").setup({
+            ensure_installed = {
+                "prettier",
+                "stylua", -- lua formatter
+                "isort", -- python formatter
+                "black", -- python formatter
+                "pylint",
+                "eslint_d",
+            },
+        })
+    end,
+  },
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
